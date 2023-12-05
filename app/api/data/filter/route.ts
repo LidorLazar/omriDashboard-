71c5b0c975d88e0878d99934a73813bc99e4db03
LidorLazar <lidorlazar33@gmail.com>
2023-12-06 00:42:32 +0200

    fix history components and NEXTAUTH

--- app/api/data/filter/route.ts
+++ app/api/data/filter/route.ts
@@ -2,7 +2,7 @@
 import {db} from "@/lib/db";
 import {NextResponse} from "next/server";
 
-export default async function handler() {
+export default async function GET() {
     // Your database query logic here
     const data = await db.delivery.findMany();
     NextResponse.json(data)
